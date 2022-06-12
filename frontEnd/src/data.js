ca446de23eb682e61c806a76b42edb73c97ce306
Asaru7 <vmlp1996@gmail.com>
2022-06-12 08:34:10 -0500

    first commit

---
+++ frontEnd/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  const products: [
+      {
+        name:'ox shirt',
+        slug:'ox-shirt',
+        category: 'shirts',
+        images: '../public/images/s1.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'ox',
+        ratin: 4.5,
+        numReview: 5,
+        description: 'Una linda camisa'
+      },
+      {
+        name:'vl shirt',
+        slug:'vl-shirt',
+        category: 'shirts',
+        images: '../public/images/s2.jpg',
+        price: 110,
+        countInStock: 5,
+        brand: 'vl',
+        ratin: 5,
+        numReview: 3,
+        description: 'Una linda camisa'
+      },
+      {
+        name:'ox pants',
+        slug:'ox-pants',
+        category: 'pants',
+        images: '../public/images/p1.jpg',
+        price: 140,
+        countInStock: 9,
+        brand: 'ox',
+        ratin: 4.2,
+        numReview: 5,
+        description: 'Comodos y resistentes'
+      },
+      {
+        name:'vl pant',
+        slug:'vl-pants',
+        category: 'pants',
+        images: '../public/images/p2.jpg',
+        price: 115,
+        countInStock: 5,
+        brand: 'vl',
+        ratin: 4.8,
+        numReview: 3,
+        description: 'De alta calidad y resistentes'
+      },
+    ]
+}
+
+export default data;
